ee49b29f9ddb5b765570d492d5ad548bf4287033
FoXire <155120948+FoXire-mov@users.noreply.github.com>
2025-07-25 01:26:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
 $(".slide").slick({
     slidesToShow: 2,
     slidesToScroll: 1,
-    dots: true,
+    dots: false,
     autoplay: true,
     autoplaySpeed: 3000, // 自動再生のスピード（ミリ秒単位）
     infinite: true, // スライドのループを有効にするか
     accessibility: true,
     arrows: true,
     swipe: true,
-    swipeToSlide: true
+    swipeToSlide: true,
+    centerMode: true
   });
 });
