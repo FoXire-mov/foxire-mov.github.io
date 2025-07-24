80084047bada3f92d39f3ef3d092e6dfa2560e43
FoXire <155120948+FoXire-mov@users.noreply.github.com>
2025-07-25 02:55:07 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 $(".slide").slick({
     slidesToShow: 2,
     slidesToScroll: 1,
+    dot: false,
     autoplay: true,
     autoplaySpeed: 3000, // 自動再生のスピード（ミリ秒単位）
     infinite: true, // スライドのループを有効にするか
