67518cac21863c155a399d993b131f21102840cc
FoXire <155120948+FoXire-mov@users.noreply.github.com>
2025-12-19 07:51:23 +0900

    Add menu toggle functionality to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+const menuBtn = document.getElementById('menu-btn');
+const spNav = document.getElementById('sp-nav');
 
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    spNav.classList.toggle('active');
+});
