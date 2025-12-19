13c673e41b1243b2dacc964008e5ef4c92420812
FoXire <155120948+FoXire-mov@users.noreply.github.com>
2025-12-19 09:34:23 +0900

    Implement menu toggle and close on link click
    Add functionality to toggle menu and close on link click

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@ const menuBtn = document.getElementById('menu-btn');
 const spNav = document.getElementById('sp-nav');
 
 menuBtn.addEventListener('click', () => {
+    // ボタンとメニューの両方に active クラスを付け外し
     menuBtn.classList.toggle('active');
     spNav.classList.toggle('active');
+});
+
+// メニュー内のリンクをクリックしたら、メニューを閉じる
+const navLinks = document.querySelectorAll('.sp-nav a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menuBtn.classList.remove('active');
+        spNav.classList.remove('active');
+    });
 });
